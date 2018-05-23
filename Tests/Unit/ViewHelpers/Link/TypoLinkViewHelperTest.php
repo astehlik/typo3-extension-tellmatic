bc6f112095c1c42cde9c34d440c510cd496c9bf8
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-05-23 16:06:13 +0200

    [BUGFIX] Initialize TSFE in TypoLinkViewHelperTest

--- Tests/Unit/ViewHelpers/Link/TypoLinkViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Link/TypoLinkViewHelperTest.php
@@ -6,6 +6,7 @@ use Nimut\TestingFramework\TestCase\ViewHelperBaseTestcase;
 use Sto\Tellmatic\ViewHelpers\Link\TypolinkViewHelper;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 class TypoLinkViewHelperTestextends extends ViewHelperBaseTestcase
 {
@@ -37,6 +38,8 @@ class TypoLinkViewHelperTestextends extends ViewHelperBaseTestcase
                 return 'linktxt';
             }
         );
+
+        $GLOBALS['TSFE'] = new TypoScriptFrontendController([], 0, 0);
 
         $this->assertEquals(
             '<a href="http://www.google.de">linktxt</a>',
