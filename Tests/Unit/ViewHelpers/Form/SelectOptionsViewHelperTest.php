c9d0ac990da22da50c34ab54267d753a62190557
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-05-23 16:06:26 +0200

    [CLEANUP] Add copyright header to tests

--- Tests/Unit/ViewHelpers/Form/SelectOptionsViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Form/SelectOptionsViewHelperTest.php
@@ -2,6 +2,16 @@
 
 namespace Sto\Tellmatic\Tests\Unit\ViewHelpers\Form;
 
+/*                                                                        *
+ * This script belongs to the TYPO3 extension "tellmatic".                *
+ *                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License, either version 3 of the   *
+ * License, or (at your option) any later version.                        *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
 use Nimut\TestingFramework\TestCase\ViewHelperBaseTestcase;
 use Sto\Tellmatic\ViewHelpers\Form\SelectOptionsViewHelper;
 
